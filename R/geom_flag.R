fcad89cc2d7ab99719ae76016d498cd16b19f225
James Goldie <james@dicenerd.app>
2023-09-30 15:38:49 +1000

    Convert uppercase values

--- R/geom_flag.R
+++ R/geom_flag.R
@@ -67,7 +67,7 @@ makeContent.flag <- function(x) {
     seq_along(x$country),
     function(ii) {
       grImport2::pictureGrob(
-        picture = .flaglist[[x$country[[ii]]]],
+        picture = .flaglist[[tolower(x$country[[ii]])]],
         x = x$x[ii], y = x$y[ii],
         width = x$size[ii] * grid::unit(1, "mm"),
         height = x$size[ii] * grid::unit(1, "mm"),
